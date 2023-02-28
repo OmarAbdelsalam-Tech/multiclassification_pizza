f535891874a736d03dddb33e9d22654cb8c45c38
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 13:03:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import base64
 
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
+    with open(backround.jpg, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
