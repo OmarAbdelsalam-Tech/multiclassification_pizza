9f63344d821004db83d89d4567c54d3d46d45725
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 12:42:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,24 +14,22 @@ import requests
 import base64
 import streamlit as st
 import base64
-open('backround.jpg', 'rb')
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="My Streamlit App")
+    st.markdown(
+        """
+        <style>
+            {css}
+        </style>
+        """.format(css=open('backround.jpg', 'rb').read()),
+        unsafe_allow_html=True
+    )
+
+if __name__ == "__main__":
+    main()
 
 
 
@@ -149,7 +147,7 @@ if button:
             st.subheader(f'ITS RAWW - Ramsey')
            
 pass
-set_background('backround.jpg')
+
 
 
                                     
