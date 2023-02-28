5e9110db4f9e5a4adda82436c288f8112f334bb3
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 13:32:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -15,30 +15,6 @@ import base64
 import streamlit as st
 import base64
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(backround.jpg, 'rb') as f:
-
-    data = f.read()
-
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/jpg;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-backround = open('backround.jpg', 'rb')
-set_png_as_page_bg('background')
-
 
 st.title("This Website uses Deep Learning to Multi-Classifies Pizza based on how cooked it is ")
 st.subheader("Contributions: Omar , Thomas, and Collin")
