e21de60d1690e3dd68f77dbaa9d6f798f279c9c9
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 12:56:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,28 +14,28 @@ import requests
 import base64
 import streamlit as st
 import base64
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-import streamlit as st
-
-main_bg = "backround.jpg"
-main_bg_ext = "jpg"
-
-side_bg = "backround.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
     <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
     </style>
-    """,
-    unsafe_allow_html=True
-)
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.jpg')
+
 
 st.title("This Website uses Deep Learning to Multi-Classifies Pizza based on how cooked it is ")
 st.subheader("Contributions: Omar , Thomas, and Collin")
