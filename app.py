09ff37ffc3b55ca94b38c193cb241a6f25c63d9c
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 13:00:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import requests
 import base64
 import streamlit as st
 import base64
+
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
@@ -25,7 +26,7 @@ def set_png_as_page_bg(png_file):
     page_bg_img = '''
     <style>
     body {
-    background-image: url("data:image/png;base64,%s");
+    background-image: url("data:image/jpg;base64,%s");
     background-size: cover;
     }
     </style>
