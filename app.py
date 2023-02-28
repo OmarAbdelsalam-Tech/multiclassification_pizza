a6d862ac8e8907b7678869c99a5230bcc9fdcd6c
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 13:04:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ import base64
 @st.cache(allow_output_mutation=True)
 def get_base64_of_bin_file(bin_file):
     with open(backround.jpg, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+
+    data = f.read()
+
+return base64.b64encode(data).decode()
 
 def set_png_as_page_bg(png_file):
     bin_str = get_base64_of_bin_file(png_file)
