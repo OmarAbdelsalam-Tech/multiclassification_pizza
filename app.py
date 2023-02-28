42d283e8cbfd90e22a06a7a0140b04be9c3b9bbb
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 12:47:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,18 +17,25 @@ import base64
 
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: open('backround.jpg', 'rb')
-    }
+main_bg = "backround.jpg"
+main_bg_ext = "jpg"
 
+side_bg = "backround.jpg"
+side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }}
+   .sidebar .sidebar-content {{
+        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
+    }}
     </style>
     """,
     unsafe_allow_html=True
 )
-
 
 st.title("This Website uses Deep Learning to Multi-Classifies Pizza based on how cooked it is ")
 st.subheader("Contributions: Omar , Thomas, and Collin")
