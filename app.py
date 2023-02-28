8b6037565ef56c4c34fd43a6050a67eb0f932ea7
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-02-28 09:13:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ if button:
          my_bar = st.progress(0, text=progress_text)
 
          for percent_complete in range(100):
-                time.sleep(0.0001)
+                time.sleep(0.01)
                 my_bar.progress(percent_complete + 1, text=progress_text)
         
                 
